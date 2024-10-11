aaad0b8970dcd487ae8f3459ceac8fd22766f5a2
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2024-10-11 09:52:40 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,20 @@
+function checkMaxLength(string, maxLength){
+    return string.length <= maxLength
+}
+// Cтрока короче 20 символов
+checkMaxLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkMaxLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkMaxLength('проверяемая строка', 10); // false
 
+function isPalindrom (string) {
+    const normalisedString = string.toLowerCase();
+    return normalisedString === normalisedString.split('').reverse().join('')
+}
+// Строка является палиндромом
+isPalindrom('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrom('ДовОд'); // true
+// Это не палиндром
+isPalindrom('Кекс');  // false
