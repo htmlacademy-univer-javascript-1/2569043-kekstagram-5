6b3a00778fb6a2028256b50b8e72e0648bf738ba
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2024-10-15 12:04:13 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,14 @@
 function checkString (string, maxLength) {
   return string.length <= maxLength;
 }
-console.log(checkString('проверяемая строка', 20)); //true
-console.log(checkString('проверяемая строка', 18)); //true
-console.log(checkString('проверяемая строка', 10)); //false
+checkString('проверяемая строка', 20); //true
+checkString('проверяемая строка', 18); //true
+checkString('проверяемая строка', 10); //false
 
 function isPalindrom (string) {
   const normalizedString = string.toLowerCase();
   return normalizedString === normalizedString.split('').reverse().join('');
 }
-console.log(isPalindrom('топот')); //true
-console.log(isPalindrom('ДовОд')); //true
-console.log(isPalindrom('Кекс')); //false
+isPalindrom('топот'); //true
+isPalindrom('ДовОд'); //true
+isPalindrom('Кекс'); //false
