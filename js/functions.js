a2028a7d11baf2a0a0c3048408c2029044631d04
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2024-10-15 11:58:41 +0500

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,23 +1,14 @@
 function checkString (string, maxLength) {
-    return string.length <= maxLength;
+  return string.length <= maxLength;
 }
-
-// Cтрока короче 20 символов
-console.log(checkString('проверяемая строка', 20)); // true
-// Длина строки ровно 18 символов
-console.log(checkString('проверяемая строка', 18)); // true
-// Строка длиннее 10 символов
-console.log(checkString('проверяемая строка', 10)); // false
-
+console.log(checkString('проверяемая строка', 20)); //true
+console.log(checkString('проверяемая строка', 18)); //true
+console.log(checkString('проверяемая строка', 10)); //false
 
 function isPalindrom (string) {
-    const normalizedString = string.toLowerCase();
-    return normalizedString === normalizedString.split('').reverse().join('');
+  const normalizedString = string.toLowerCase();
+  return normalizedString === normalizedString.split('').reverse().join('');
 }
-
-// Строка является палиндромом
-console.log(isPalindrom('топот')); // true
-// Несмотря на разный регистр, тоже палиндром
-console.log(isPalindrom('ДовОд')); // true
-// Это не палиндром
-console.log(isPalindrom('Кекс'));  // false
+console.log(isPalindrom('топот')); //true
+console.log(isPalindrom('ДовОд')); //true
+console.log(isPalindrom('Кекс')); //false
