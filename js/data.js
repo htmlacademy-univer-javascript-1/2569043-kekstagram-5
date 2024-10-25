7e0fa7a7d105a91f0371aa7dc9e6f6e324667809
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2024-10-25 08:27:50 +0500

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,42 @@
+import {
+  getRandomInteger,
+  createRandomId
+} from './util.js';
+const comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const names = [
+  'Артём',
+  'Ирина',
+  'Дмитрий',
+  'Екатерина',
+  'Алексей',
+  'Мария',
+  'Иван'
+];
+const descr = [
+  'Утренний кофе с видом на город.',
+  'Портрет любимого питомца.',
+  'Культурное событие в центре города.',
+  'Летнее утро в цветущем саду.',
+  'Архитектурные детали красивого здания.',
+  'Кадр уличного искусства в старом районе.'
+];
+export const createComments = () => ({
+  id: createRandomId(1, 750),
+  avatar: `img/avatar-${getRandomInteger(1, 6)}.jpg`,
+  message: comments[getRandomInteger(0, comments.length - 1)],
+  name: names[getRandomInteger(0, names.length - 1)],
+});
+export const createPhotoDescr = () => ({
+  id: createRandomId(1, 25),
+  url: `photos/${getRandomInteger(1, 25)}.jpg`,
+  description: descr[getRandomInteger(0, descr.length - 1)],
+  likes: createRandomId(15, 200),
+  comments: Array.from({length: getRandomInteger(0, 30)}, createComments)
+});
