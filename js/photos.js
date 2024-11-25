18f43ac733ca62803b528a253bca59b809fc8578
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2024-11-25 10:31:41 +0500

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -1,16 +1,16 @@
 import {createPhotoDescr} from './data.js';
 const pictureContainer = document.querySelector('.pictures');
 const photoTemplate = document.querySelector('#picture')
- .content
- .querySelector('.picture');
+  .content
+  .querySelector('.picture');
 const photos = createPhotoDescr();
 const photoFragment = document.createDocumentFragment();
 photos.forEach(({url, description, likes, comments}) => {
- const newPhoto = photoTemplate.cloneNode(true);
- newPhoto.querySelector('.picture__img').src = url;
- newPhoto.querySelector('.picture__img').alt = description;
- newPhoto.querySelector('.picture__img').textContent = likes;
- newPhoto.querySelector('.picture__img').textContent = comments.length;
- photoFragment.appendChild(newPhoto);
- })
+  const newPhoto = photoTemplate.cloneNode(true);
+  newPhoto.querySelector('.picture__img').src = url;
+  newPhoto.querySelector('.picture__img').alt = description;
+  newPhoto.querySelector('.picture__img').textContent = likes;
+  newPhoto.querySelector('.picture__img').textContent = comments.length;
+  photoFragment.appendChild(newPhoto);
+});
 pictureContainer.appendChild(photoFragment);
