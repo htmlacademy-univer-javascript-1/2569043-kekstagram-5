20319e578fa623ebc10038d59e9e9022277d8210
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2024-12-04 17:23:49 +0500

    Update photos.js

--- js/photos.js
+++ js/photos.js
@@ -1,16 +1,22 @@
 import {createPhotoDescr} from './data.js';
+import {openPost} from './post.js';
 const pictureContainer = document.querySelector('.pictures');
-const photoTemplate = document.querySelector('#picture')
-  .content
-  .querySelector('.picture');
-const photos = createPhotoDescr();
-const photoFragment = document.createDocumentFragment();
-photos.forEach(({url, description, likes, comments}) => {
+const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const renderPhoto = (post) => {
   const newPhoto = photoTemplate.cloneNode(true);
-  newPhoto.querySelector('.picture__img').src = url;
-  newPhoto.querySelector('.picture__img').alt = description;
-  newPhoto.querySelector('.picture__img').textContent = likes;
-  newPhoto.querySelector('.picture__img').textContent = comments.length;
-  photoFragment.appendChild(newPhoto);
-});
-pictureContainer.appendChild(photoFragment);
+  newPhoto.querySelector('.picture__img').src = post.url;
+  newPhoto.querySelector('.picture__comments').textContent = post.comments.length;
+  newPhoto.querySelector('.picture__likes').textContent = post.likes;
+  newPhoto.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    openPost();
+  });
+  return newPhoto;
+};
+export const renderPrew = function() {
+  const photoFragment = document.createDocumentFragment();
+  createPhotoDescr.forEach((pictureContainer) => {
+    photoFragment.appendChild(renderPhoto(pictureContainer));
+  });
+  pictureContainer.appendChild(photoFragment);
+};
