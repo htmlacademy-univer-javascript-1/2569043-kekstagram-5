53041e699e1901beee3cf278c5e67abf2c009edb
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-01-03 23:07:31 +0500

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,14 +1,20 @@
 import {isKeyEsc} from './util.js';
+import {reset} from './effects-image.js';
 const maxTagsCount = 5;
 const tagsRule = /^#[a-za-яё0-9]{1,19}$/i;
 const tagError = 'Не верно введены хэштеги';
-const form = document.querySelector('#upload-select-image');
+const form = document.querySelector('.img-upload__form');
+const submitBtn = form.querySelector('.img-upload__submit');
 const tagsField = document.querySelector('.text__hashtags');
 const commField = document.querySelector('.text__description');
 const file = document.querySelector('#upload-file');
 const overlay = document.querySelector('.img-upload__overlay');
 const body = document.querySelector('.body');
 const cancelBtn = document.querySelector('#upload-cancel');
+const success = document.querySelector('#success').content.querySelector('.success');
+const successBtn = document.querySelector('#success').content.querySelector('.success__button');
+const error = document.querySelector('#error').content.querySelector('.error');
+const errorBtn = document.querySelector('#error').content.querySelector('.error__button');
 const pristine = new Pristine(form, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
@@ -21,17 +27,21 @@ const openModal = () => {
 };
 const closeModal = () => {
   form.reset();
+  reset();
   pristine.reset();
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
 };
-const ifInText = () =>
-  document.activeElement === tagsField || document.activeElement === commField;
+const ifInTextFieldFocused = () =>
+  document.activeElement === hashtagField || document.activeElement === commentField;
 function onDocumentKeydown(evt) {
-  if (isKeyEsc(evt) && ifInText()) {
+  if (isKeyEsc(evt) && !ifInTextFieldFocused()) {
     evt.preventDefault();
-    closeModal();
+    const hasHiddenPopup = document.querySelector('.error');
+    if (!hasHiddenPopup) {
+      closeModal();
+    }
   }
 }
 const cancelBtnClick = () => {
