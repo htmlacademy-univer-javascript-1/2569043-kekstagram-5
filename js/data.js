043945aafff0e34773183f3b36637cb1d3b5e718
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-01-04 11:47:22 +0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -52,3 +52,9 @@ export function createArrayOfPhotos () {
       photoArray.push(createPhoto());
   }
 }
+export const pristine = new pristine(document.querySelector('.img-upload__form'), {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'p',
+  errorTextClass: 'img-upload__error'
+}, true);
