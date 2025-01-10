5a5adc3907de791c80751b0ff4455c4c292776d1
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-01-10 16:53:31 +0500

    Update and rename submit_form.js to submit-form.js

--- js/submit-form.js
+++ js/submit-form.js
@@ -1,8 +1,10 @@
 import {closeForm} from './form.js';
-import {isKeyEsc } from './util.js';
+import {isKeyEsc} from './util.js';
+
 const body = document.body;
 const errorMessage = body.querySelector('#error').content.querySelector('section');
 const successMessage = document.querySelector('#success').content.querySelector('section');
+
 const mainClick = (evt) => {
   const clickElem = evt.target;
   if(clickElem.classList.contains('success__inner') || clickElem.classList.contains('error__inner')){
@@ -10,17 +12,20 @@ const mainClick = (evt) => {
   }
   closeMessage();
 };
+
 const mainKeyDown = (evt) => {
   evt.preventDefault();
-  if(isKeyEsc(evt)){
+  if(isKeyEsc (evt)){
     closeMessage();
   }
 };
+
 function closeMessage () {
   body.removeEventListener('click', mainClick);
   document.removeEventListener('keydown', mainKeyDown);
   body.removeChild(body.lastChild);
 }
+
 const showMessage = (messageTemplate) => {
   const message = messageTemplate.cloneNode(1);
   message.style.zIndex = 100;
@@ -28,10 +33,12 @@ const showMessage = (messageTemplate) => {
   body.addEventListener('click', mainClick);
   body.appendChild(message);
 };
+
 export const formSuccess = () => {
   closeForm();
   showMessage(successMessage);
 };
+
 export const formFail = () => {
   showMessage(errorMessage);
 };
