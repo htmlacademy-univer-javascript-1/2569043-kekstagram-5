6f39e948e4554ef4980f2f1dc257e817dbdc7dac
BlackWolf85 <147985619+BlackWolf85@users.noreply.github.com>
2025-01-10 16:49:57 +0500

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,30 +1,36 @@
 import {debounce, shuffle} from './util.js';
-import {createPhoto, removePhoto} from './photos.js';
 import {photos} from './main.js';
-const imgFilterElement = document.querySelector('.img-filters__form');
-let activeFilter = document.querySelector('img-filters__button--active');
-const maxPhotoNum = 10;
+import {createPhoto, removePhoto} from './photos.js';
+
+const imageFilterElement = document.querySelector('.img-filters__form');
+let activeFilter = document.querySelector('.img-filters__button--active');
+const MAX_PHOTO_NUMBER = 10;
+
 const mainFilters = {
   'filter-default': () => photos.slice(),
-  'filter-random': () => shuffle(photos.slice()).slice(0, maxPhotoNum),
+  'filter-random': () => shuffle(photos.slice()).slice(0, MAX_PHOTO_NUMBER),
   'filter-discussed': () => photos.slice().sort((first, second) => second.comments.length - first.comments.length),
 };
+
 const apply = (id) =>{
   removePhoto();
   createPhoto(mainFilters[id]());
 };
-const toogleBtn = (evt) => {
+
+const toogleButton = (evt) => {
   activeFilter.classList.remove('img-filters__button--active');
   activeFilter = evt.target;
   activeFilter.classList.add('img-filters__button--active');
 };
-const filterClick = debounce((evt) => {
+
+const onFilterClick = debounce((evt) => {
   evt.preventDefault();
   if(evt.target.type === 'button'){
     apply(evt.target.id);
-    toogleBtn(evt);
+    toogleButton(evt);
   }
 });
+
 export const initFilters = () => {
-  imgFilterElement.addEventListener('click', filterClick);
+  imageFilterElement.addEventListener('click', onFilterClick);
 };
